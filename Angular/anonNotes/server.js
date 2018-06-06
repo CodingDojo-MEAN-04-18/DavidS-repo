9bbc08c8b1cc955ec893c95d7ab489c00b368e73
Your Name <dszzz2017@gmail.com>
2018-06-05 18:36:48 -0700

    Integration - Anonymous Notes

---
+++ Angular/anonNotes/server.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const port = process.env.PORT || 6780;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+require('./server/config/database');
+
+app.use('/api', require('./server/config/routes'));
+
+app.use(require('./server/config/routes/catch-all.route'));
+
+app.listen(port, () => console.log(`express server listening on port ${port}`));
---
+++ Angular/anonNotes/src/app/note.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { NoteService } from './note.service';
+
+describe('NoteService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [NoteService]
+    });
+  });
+
+  it('should be created', inject([NoteService], (service: NoteService) => {
+    expect(service).toBeTruthy();
+  }));
+});
+© 2018 GitHub, Inc.
